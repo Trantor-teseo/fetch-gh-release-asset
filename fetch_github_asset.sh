664b9e598e34e91468b2f75dca72e58dfd5b0196
Alessandro Sperinde <alessandro.sperinde@teseotech.com>
2020-11-09 17:39:30 +0100

    Added headers to REST request

--- fetch_github_asset.sh
+++ fetch_github_asset.sh
@@ -21,10 +21,13 @@ if ! [[ -z ${INPUT_TOKEN} ]]; then
   TOKEN=$INPUT_TOKEN
 fi
 
+HEADERS="-H \"Accept: application/vnd.github.v3+json\""
 API_URL="https://$TOKEN:@api.github.com/repos/$REPO"
-RELEASE_DATA=$(curl $API_URL/releases/${INPUT_VERSION})
+RELEASE_DATA=$(curl $HEADERS $API_URL/releases/${INPUT_VERSION})
 ASSET_ID=$(echo $RELEASE_DATA | jq -r ".assets | map(select(.name == \"${INPUT_FILE}\"))[0].id")
 TAG_VERSION=$(echo $RELEASE_DATA | jq -r ".tag_name" | sed -e "s/^v//" | sed -e "s/^v.//")
+echo "API_URL"
+echo "$API_URL"
 echo "RELEASE DATA:"
 echo "$RELEASE_DATA"
 echo "ASSET ID:"
